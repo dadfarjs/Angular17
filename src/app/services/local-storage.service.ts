93a2d4e1fde4d859bb051b95ac71b16783eb6807
mostafa dadfar <dadfarjs@gmail.com>
2024-05-22 20:07:45 +0330

    DI & service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as CryptoJS from 'crypto-js';
+
+const SECRET_KEY = '9c789cNC9t78v96BHI23n9y82SDkF49028bt7';
+
+@Injectable()
+export class LocalStorageService {
+  set(key: string, value: unknown) {
+    const enctypted = CryptoJS.AES.enctypt(
+      JSON.stringify(value),
+      SECRET_KEY
+    ).toString();
+    return localStorage.setItem(key, enctypted);
+  }
+
+  get(key: string) {
+    const encrypted = localStorage.getItem(key) || '';
+    const dectypted = CryptoJS.AES.decrypt(encrypted, SECRET_KEY).toString(
+      CryptoJS.enc.Utf8
+    );
+    return encrypted ? JSON.parse(dectypted) : {};
+  }
+
+  remove(key: string) {
+    return localStorage.removeItem(key);
+  }
+
+  clear() {
+    return localStorage.clear();
+  }
+}
